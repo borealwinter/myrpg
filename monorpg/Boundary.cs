7a6044a18bda6499fce700a58bb6c765fee2b002
borealwinter <winterstorm1024@gmail.com>
2015-09-09 22:31:25 -0700

    Began work on Boundary class

---
+++ monorpg/Boundary.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledSharp;
+
+namespace monorpg
+{
+    /// <summary>
+    /// An obstacle that entities normally cannot pass through, such as a wall.
+    /// </summary>
+    public class Boundary : MapObject
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Boundary()
+        {
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+    }
+}
